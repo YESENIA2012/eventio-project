b3abcfaa9b93a85e47f41c2f2ce94a7527e6d7cb
YESENIA2012 <gyesenia173@gmail.com>
2022-11-21 15:26:42 -0500

    created the Login and Image components

--- src/Components/image/ImageContainer.js
+++ src/Components/image/ImageContainer.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Hidden } from "@mui/material";
+import "./imageStyle.scss";
 
 const Image = () => {
   return (
     <Hidden smDown>
+      <span className="span-1">E.</span>
       <img
         src="https://raw.githubusercontent.com/erick2014/events-app/master/src/assets/images/starWars.png"
         alt="star wars stormtroopers"
       />
+      <div className="text-image">
+        <p className="p-image">“Great, kid. Don't get cocky.”</p>
+        <span className="span-2">_</span>
+        <p className="p-image-2">Han solo</p>
+      </div>
     </Hidden>
   );
 };
--- src/Components/login/login.js
+++ src/Components/login/login.js
@@ -1,7 +1,16 @@
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, ThemeProvider, createTheme } from "@mui/material";
 import React, { useState, Fragment } from "react";
-import Image from "../ImageContainer";
+import Image from "../image/ImageContainer";
 import { makeStyles } from "tss-react/mui";
+import "./styleLogin.scss";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "rgb(100, 207, 140)",
+    },
+  },
+});
 
 const styles = makeStyles()((theme) => {
   return {
@@ -22,53 +31,55 @@ const Login = () => {
   const { classes } = styles();
 
   return (
-    <Fragment>
-      <div className="login">
-        <Image />
-        <div className="section-login">
-          <div className="sign-in-button">
-            <span>Don’t have account?</span>
-            <button>SIGN UP</button>
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <div className="login">
+          <Image />
+          <div className="section-login">
+            <div className="sign-in-button">
+              <span>Don’t have account?</span>
+              <button className="button-s">SIGN UP</button>
+            </div>
+            <div className="title-and-message">
+              <h1 className="title-login">Sing in to Eventio.</h1>
+              <p className="message-login">{messageSignIn}</p>
+            </div>
+            <form className="form-login">
+              <TextField
+                id="textfield"
+                label="Email"
+                variant="standard"
+                type="email"
+                className={classes.textFieldStyle}
+                InputLabelProps={{ className: "textfield-label" }}
+                name="email"
+                sx={{
+                  "& .MuiInputLabel-root": {},
+                  borderBottom: "1px solid rgb(179, 175, 177)",
+                }}
+                InputProps={{ disableUnderline: true }}
+              ></TextField>
+              <TextField
+                id="textfield"
+                label="Password"
+                variant="standard"
+                className={classes.textFieldStyle}
+                InputLabelProps={{ className: "textfield-label" }}
+                name="password"
+                sx={{
+                  "& .MuiInputLabel-root": {},
+                  borderBottom: "1px solid rgb(179, 175, 177)",
+                }}
+                InputProps={{ disableUnderline: true }}
+              ></TextField>
+              <Button className="button-login" variant="contained">
+                SIGN IN
+              </Button>
+            </form>
           </div>
-          <div className="title-and-message">
-            <h1 className="title-login">Sing in to Eventio.</h1>
-            <p className="message-login">{messageSignIn}</p>
-          </div>
-          <form className="form-login">
-            <TextField
-              id="textfield"
-              label="Email"
-              variant="standard"
-              type="email"
-              className={classes.textFieldStyle}
-              InputLabelProps={{ className: "textfield-label" }}
-              name="email"
-              sx={{
-                "& .MuiInputLabel-root": {},
-                borderBottom: "1px solid rgb(179, 175, 177)",
-              }}
-              InputProps={{ disableUnderline: true }}
-            ></TextField>
-            <TextField
-              id="textfield"
-              label="Password"
-              variant="standard"
-              className={classes.textFieldStyle}
-              InputLabelProps={{ className: "textfield-label" }}
-              name="password"
-              sx={{
-                "& .MuiInputLabel-root": {},
-                borderBottom: "1px solid rgb(179, 175, 177)",
-              }}
-              InputProps={{ disableUnderline: true }}
-            ></TextField>
-            <Button className="button-login" variant="contained">
-              SIGN IN
-            </Button>
-          </form>
         </div>
-      </div>
-    </Fragment>
+      </Fragment>
+    </ThemeProvider>
   );
 };
 
