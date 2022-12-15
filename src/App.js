8de8a524db516a60feea451cf867d98c8a35e19d
YESENIA2012 <gyesenia173@gmail.com>
2022-12-15 18:17:23 -0500

    removed mixins and variables from app.scss file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import Login from "./components/login/Login";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import SignUp from "./components/signUp/SignUp";
-import DashboardComponent from "./components/dashboard/Dashboard";
 import "./styles/app.scss";
 
 function App() {
@@ -10,8 +8,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="sign-up" element={<SignUp />} />
-        <Route path="dashboard" element={<DashboardComponent />} />
       </Routes>
     </div>
   );
