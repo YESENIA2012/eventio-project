3ee7f1bae633b46a7d265ab797af0f4b2f92a409
YESENIA2012 <gyesenia173@gmail.com>
2022-12-28 12:47:39 -0500

    The Error404 component was implemented.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/login/Login";
 import SignUp from "./components/signUp/SignUp";
 import Dashboard from "./components/dashboard/Dashboard";
-import NewEvent from "./components/newEvent/NewEvent";
-import EditEvent from "./components/editEvent/EditEvents";
 import Error404 from "./components/error/Error404";
 import "./styles/app.scss";
 
@@ -12,11 +9,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/createEvent" element={<NewEvent />} />
-        <Route path="/EditEvent" element={<EditEvent />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
